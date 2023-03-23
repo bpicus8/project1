dd7b39cf4e36a6585037ae54c98a6405bb88faec
bpicus08 <bpicus@hawk.iit.edu>
2023-03-23 11:14:38 -0500

    added another jquery

--- js/site.js
+++ js/site.js
@@ -2,3 +2,11 @@ jQuery(function($) {
 	$('html').removeClass('nojs');
 	$('html').addClass('hasjs');
 });
+
+ 
+$('form').submit(function() {
+ if ($('#my-input').val() === '') {
+ alert('Please fill in the required field!'); 
+ return false; // Prevent form submission 
+} 
+}); 
