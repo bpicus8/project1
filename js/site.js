db59c5882d6c0aa67d678c46488be3f6f9462a64
bpicus08 <104456856+bpicus08@users.noreply.github.com>
2023-03-23 14:45:13 -0500

    final site.js cascade

--- js/site.js
+++ js/site.js
@@ -1,12 +1,12 @@
 jQuery(function($) {
-	$('html').removeClass('nojs');
+        $('html').removeClass('nojs');
 	$('html').addClass('hasjs');
 });
 
  
 $('form').submit(function() {
- if ($('#my-input').val() === '') {
- alert('Please fill in the required field!'); 
- return false; // Prevent form submission 
-} 
+	if ($('#my-input').val() === '') {
+		alert('Please fill in the required field!'); 
+		return false; // Prevent form submission
+	} 
 }); 
